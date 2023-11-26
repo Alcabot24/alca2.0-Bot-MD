0507102ea39cfb845d77332f04431cee8b40d2fd
Alcabot <144275249+Alcabot24@users.noreply.github.com>
2023-11-25 19:51:44 -0600

    Actualizar menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -21,7 +21,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = ` 𝗔𝗟𝗖𝗔𝗕𝗢𝗧𝗩𝟮.𝟬
+    const str = ` 𝗡𝗔𝗝𝗘𝗥𝗔 𝗕𝗢𝗧
 
  ┏━━━━━━━━━━━━━━━━━━┓
 ┃ *◉—𝗡𝗔𝗝𝗘𝗥𝗔 𝗕𝗢𝗧—◉*

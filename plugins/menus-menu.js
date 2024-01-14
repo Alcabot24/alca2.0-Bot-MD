3bff2838c8cd1c05c45b36bb07fe3dd3c48c96c5
Alcabot <144275249+Alcabot24@users.noreply.github.com>
2024-01-14 17:33:59 -0600

    Actualizar menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -21,101 +21,71 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = ` 𝗔𝗟𝗖𝗔𝗕𝗢𝗧|𝟰
-
- ┏━━━━━━━━━━━━━━━━━━┓
-┃ *◉—𝗔𝗟𝗖𝗔 𝗕𝗢𝗧—◉*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+    const str = `
+┏━━━━━━━━━━━━━━━━━━┓
+┃ *◉—𝘼𝙇𝘾𝘼²|𝘽𝙊𝙏—◉*
 ┣⟣☯︎ *𝗢𝗪𝗡𝗘𝗥:* ALCAVENTAS
 ┣⟣☯︎ *𝗡𝗨𝗠𝗘𝗥𝗢:* +52 824 105 0228
 ┗━━━━━━━━━━━━━━━━━━┛
-
-*┃𝗕 𝗢 𝗧 𝗠 𝗘 𝗡 𝗨 𝗔𝗗𝗠𝗜𝗡𝗜𝗦𝗧𝗥𝗔𝗗𝗢𝗥𝗘𝗦*┃
 ┏━━━━━━━━━━━━━━━━┓
-*┃ ✨𝗔𝗖𝗧𝗜𝗩𝗔𝗥 𝗢 𝗗𝗘𝗦𝗔𝗖𝗧𝗜𝗩𝗔𝗥✨*
+*┃✨𝗔𝗖𝗧𝗜𝗩𝗔𝗥 𝗢 𝗗𝗘𝗦𝗔𝗖𝗧𝗜𝗩𝗔𝗥✨*
 ┃Enable(activar) Disable(desactivar)┃
-┣⚙️ _.enable *welcome*_
-┣⚙️ _.disable *welcome*_
-┣⚙️ _.enable *antiviewonce*_
-┣⚙️ _.disable *antiviewonce*_
-┣⚙️ _.enable *modoadmin*_
-┣⚙️ _.disable *modoadmin*_
+┣⚙️ _.enable *welcome*_ (Activar Bienvenidas)
+┣⚙️ _.disable *welcome*_ (Desactivar Bienvenida)
+┣⚙️ _.enable *modoadmin*_ (Activar solo admis)
+┣⚙️ _.disable *modoadmin*_ (Desactivar solo admis)
 ┗━━━━━━━━━━━━━━━━┛
 ┏━━━━━━━━━━━━━━━━┓
-*┃ ✨𝗔𝗝𝗨𝗦𝗧𝗘𝗦 𝗗𝗘 𝗚𝗥𝗨𝗣𝗢𝗦 ✨* 
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
-┣⚙️ _.add *<numero>*_
-┣⚙️ _.kick *<@tag>*_
+*┃✨𝗔𝗝𝗨𝗦𝗧𝗘𝗦 𝗗𝗘 𝗚𝗥𝗨𝗣𝗢𝗦 ✨* 
 ┣⚙️ _.kick2 *<@tag>*_
-┣⚙️ _.listanum *<texto>*_
 ┣⚙️ _.kicknum *<texto>*_
 ┣⚙️ _.grupo *<abrir / cerrar>*_
-┣⚙️ _.grouptime *<opcion> <tiempo>*_
-┣⚙️ _.promote *<@tag>*_
-┣⚙️ _.demote *<@tag>*_
-┣⚙️ _admins *<texto>*_ (𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)
-┣⚙️ _.demote *<@tag>*_
+┣⚙️ _.promote *<@tag>*_ (Dar admi)
+┣⚙️ _.demote *<@tag>*_ (Quitar admi)
 ┣⚙️ _.infogroup_
-┣⚙️ _.resetlink_
 ┣⚙️ _.link_
-┣⚙️ _.setname *<texto>*_
-┣⚙️ _.setdesc *<texto>*_
-┣⚙️ _.invocar *<texto>*_
-┣⚙️ _.setwelcome *<texto>*_
-┣⚙️ _.setbye *<texto>*_
-┣⚙️ _.hidetag *<texto>*_
-┣⚙️ _.hidetag *<audio>*_
-┣⚙️ _.hidetag *<video>*_
-┣⚙️ _.hidetag *<imagen>*_
-┣⚙️ _.warn *<@tag>*_
-┣⚙️ _.unwarn *<@tag>*_
-┣⚙️ _.listwarn_
+┣⚙️ _.todos *<texto>*_ (Mencion a todos)
+┣⚙️ _.setwelcome *<texto>*_ (Editar bienvenida)
+┣⚙️ _.setbye *<texto>*_ (Editar Despedida)
+┣⚙️ _.notify_ (Hacer notifys)
 ┣⚙️ _.fantasmas_
-┣⚙️ _.destraba_
-┣⚙️ _.setpp *<imagen>*_
 ┗━━━━━━━━━━━━━━━━┛
 ┏━━━━━━━━━━━━━━━━┓
-*┃✨ 𝗕𝗨𝗦𝗖𝗔𝗥 ✨*
-┃ 𝗔𝗟𝗖𝗔𝗕𝗢𝗧|𝟰 ≡┃
-┣🟢 _.githubsearch *<texto>*_
-┣🟢 _.pelisplus *<texto>*_
-┣🟢 _.modapk *<texto>*_
-┣🟢 _.stickersearch *<texto>*_
-┣🟢 _.stickersearch2 *<texto>*_
-┣🟢 _.animeinfo *<texto>*_
-┣🟢 _.google *<texto>*_
+*┃✨𝗕𝗨𝗦𝗖𝗔𝗥✨*
+┣🔍 _.githubsearch *<texto>*_
+┣🔍 _.pelisplus *<texto>*_
+┣🔍 _.modapk *<texto>*_
+┣🔍 _.stickersearch *<texto>*_
+┣🔍 _.stickersearch2 *<texto>*_
+┣🔍 _.animeinfo *<texto>*_
+┣🔍 _.google *<texto>*_
 ┗━━━━━━━━━━━━━━━━┛
 ┏━━━━━━━━━━━━━━━━┓
-*┃✨ 𝗙𝗥𝗔𝗦𝗘𝗦 𝗢 𝗣𝗜𝗥𝗢𝗣𝗢𝗦✨*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
-┣⚫️ _.piropo_
-┣⚫️ _.consejo_
-┣⚫️ _.fraseromantica_
-┣⚫️ _.historiaromantica_
-┗━━━━━━━━━━━━━━━━┛
-
-┏━━━━━━━━━━━━━━━━┓
-*┃✨ 𝗛𝗘𝗥𝗥𝗔𝗠𝗜𝗘𝗡𝗧𝗔𝗦✨*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
-┣🟠 _.clima *<país> <ciudad>*_
-┣🟠 _.encuesta *<texto1|texto2...>*_
-┣🟠 _.ocr *<responde a imagen>*_
-┣🟠 _.hd *<responde a imagen>*_
-┣🟠 _.acortar *<enlace / link / url>*_
-┣🟠 _.calc *<operacion math>*_
-┣🟠 _.horario_
+*┃✨𝗙𝗥𝗔𝗦𝗘𝗦 𝗢 𝗣𝗜𝗥𝗢𝗣𝗢𝗦✨*
+┣📖 _.piropo_
+┣📖 _.consejo_
+┣📖 _.fraseromantica_
+┣📖 _.historiaromantica_
 ┗━━━━━━━━━━━━━━━━┛
 ┏━━━━━━━━━━━━━━━━┓
-*┃ ✨𝗦𝗧𝗜𝗖𝗞𝗘𝗥𝗦✨*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
-┣🟣 _.sticker *<responder a imagen o video>*_
-┣🟣 _.kiss *<@tag>*_
-┣🟣 _.dado_
+*┃✨𝗛𝗘𝗥𝗥𝗔𝗠𝗜𝗘𝗡𝗧𝗔𝗦✨*
+┣🔧 _.clima *<país> <ciudad>*_
+┣🔧 _.encuesta *<texto1|texto2...>*_
+┣🔧 _.ocr *<responde a imagen>*_
+┣🔧 _.hd *<responde a imagen>*_
+┣🔧 _.acortar *<enlace / link / url>*_
+┣🔧 _.calc *<operacion math>*_
+┣🔧 _.horario_
+┗━━━━━━━━━━━━━━━━┛
+┏━━━━━━━━━━━━━━━━┓
+*┃✨𝗦𝗧𝗜𝗖𝗞𝗘𝗥𝗦✨*
+┣🍭 _.sticker *<responder a imagen o video>*_
+┣🍭 _.HD *<responder a imagen
 ┗━━━━━━━━━━━━━━━━┛
 ┏━━━━━━━━━━━━━━━━┓
 *┃ ✨ 𝗗𝗘𝗦𝗖𝗔𝗥𝗚𝗔𝗦 ✨*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
-┣🟣 _.play *<texto de la canción >*_
+┣🏓 _.play *<texto de la canción >*_
+┣🚨 No Descargar Music (evitar bugeos)
 ┗━━━━━━━━━━━━━━━━┛
 𝘾𝙍𝙀𝘼𝘿𝙊𝙍 @𝘼𝙇𝘾𝘼𝙎𝙃𝙊𝙋 𝘾𝙐𝘼𝙇𝙌𝙐𝙄𝙀𝙍 𝘿𝙐𝘿𝘼 𝙈𝘼𝙉𝘿𝘼𝙍𝙈𝙀 𝙈𝙀𝙉𝙎𝘼𝙅𝙀 𝘼𝙇 𝙒𝙋𝙋 +5282410502208`.trim();
     if (m.isGroup) {
